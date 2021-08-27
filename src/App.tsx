5617c0eda487e83431381c5f879eeca5fc1f3499
HF77554 <79154786+HF77554@users.noreply.github.com>
2021-08-27 14:44:40 -0400

    Added Form Selectors you can now select category, difficulty, & amount of questions to showcase on site

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import "./styles.css";
 import CardQuizMain from "./CardQuizMain";
 import FlashCardSelectorForm from "./Components/FlashCardSelectorForm";
+import { useState } from "react";
 
 export default function App() {
+  const [APIurl, APIurlHandler] = useState(
+    "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium"
+  );
+
+  const apiSelectorHandler = (category, amount, difficulty, e) => {
+    e.preventDefault();
+    const categoryID = category.current.value;
+    const numQuestions = amount.current.value;
+    const questionDifficulty = !difficulty.current.value
+      ? ""
+      : `&difficulty=${difficulty.current.value}`;
+    APIurlHandler(
+      `https://opentdb.com/api.php?amount=${numQuestions}&category=${categoryID}${questionDifficulty}`
+    );
+  };
+
   return (
-    <div>
-      <FlashCardSelectorForm />
-      <CardQuizMain />
+    <div className="app_container">
+      <FlashCardSelectorForm onSelection={apiSelectorHandler} />
+      <CardQuizMain APIurl={APIurl} />
     </div>
   );
 }
--- src/CardQuizMain.tsx
+++ src/CardQuizMain.tsx
@@ -14,32 +14,35 @@ const decodeString = (str: string) => {
   return textArea.value;
 };
 
-export default function CardQuizMain() {
+export default function CardQuizMain({ APIurl }) {
   const [flashCards, flashCardsHandler] = useState([]);
 
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium")
-      .then((response) => response.json())
-      .then((data) =>
-        flashCardsHandler(
-          data.results.map((questionItem, index) => {
-            const answer = decodeString(questionItem.correct_answer);
-            const options = [
-              ...questionItem.incorrect_answers.map((a: string) =>
-                decodeString(a)
-              ),
-              answer
-            ];
-            return {
-              id: `${index}-${Date.now()}`,
-              question: decodeString(questionItem.question),
-              answer: answer,
-              wrongOptions: options.sort(() => Math.random() - 0.5)
-            };
-          })
-        )
-      );
-  }, []);
+    const fetchAPI = async (url: string) => {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      //cleans data and formats it to fit into flashcard format
+      const flashCards = data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer);
+        const options = [
+          ...questionItem.incorrect_answers.map((a: string) => decodeString(a)),
+          answer
+        ];
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          wrongOptions: options.sort(() => Math.random() - 0.5)
+        };
+      });
+
+      //updates flashcard state
+      flashCardsHandler(flashCards);
+    };
+
+    fetchAPI(APIurl);
+  }, [APIurl]);
 
   return (
     <div className="container">
